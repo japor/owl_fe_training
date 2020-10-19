49a2a084900944d25a4e22a496e42868bdc74b1e
japor <jpr.mejoy@gmail.com>
2020-10-19 23:37:04 +0800

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode:  "development",
+    entry: "./src/scripts/app.js",
+    output: {
+        filename: "app.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+    plugins : [
+        new CopyPlugin({
+            patterns: [
+                { 
+                    from: 'images', 
+                    to: 'images',
+                    context: 'src/',
+                    flatten: true
+                },
+                { 
+                    from: 'fonts', 
+                    to: 'fonts',
+                    context: 'src/',
+                }
+            ],
+        }),
+]
+}
